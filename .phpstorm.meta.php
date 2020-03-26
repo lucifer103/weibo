035865eb1b51ea1786551ff0e9644f2907354fa8
Lucifer <hug.m@foxmail.com>
2020-03-26 22:38:15 +0800

    move fzaninotto/faker package from require-dev to require

--- .phpstorm.meta.php
+++ .phpstorm.meta.php
@@ -5,7 +5,7 @@ namespace PHPSTORM_META {
 
    /**
     * PhpStorm Meta file, to provide autocomplete information for PhpStorm
-    * Generated on 2020-03-25 21:12:49.
+    * Generated on 2020-03-26 14:36:42.
     *
     * @author Barry vd. Heuvel <barryvdh@gmail.com>
     * @see https://github.com/barryvdh/laravel-ide-helper
